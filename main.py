e66e9d1cc337339fc379d0adc22d31bf7a7cc08c
Yudi Asano Ramos <71808184+Yudiaramos@users.noreply.github.com>
2024-02-15 23:53:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import openpyxl
+import openpyxl #biblioteca que permite usra arquivos tipo xlsx em vez de csv
+
+#definimos 3 funções que são as possibilidades das notas
 
 def calcular_media(p1, p2, p3):
     return (float(p1) + float(p2) + float(p3)) / 3
@@ -10,11 +12,12 @@ def reprovado_por_falta(total_faltas, total_aulas):
     return total_faltas > 0.25 * total_aulas
 
 def calcular_situacao_aluno(linha):
+    #sheet -> pega os valores das colunas e linhas especificadas
     p1 = sheet.cell(row=linha, column=4).value
     p2 = sheet.cell(row=linha, column=5).value
     p3 = sheet.cell(row=linha, column=6).value
     total_faltas = sheet.cell(row=linha, column=3).value
-    total_aulas = 60  # Substitua pelo número real de aulas
+    total_aulas = 60  
 
     media = calcular_media(p1, p2, p3)
 
@@ -25,20 +28,20 @@ def calcular_situacao_aluno(linha):
             sheet.cell(row=linha, column=7).value = 'Reprovado por Nota'
         elif 50 <= media < 70:
             sheet.cell(row=linha, column=7).value = 'Exame Final'
-            # Calcular a Nota para Aprovação Final (naf)
+            # caulo a Nota para Aprovação Final (naf)
             naf = calcular_naf(media)
             sheet.cell(row=linha, column=8).value = naf
         else:
             sheet.cell(row=linha, column=7).value = 'Aprovado'
             sheet.cell(row=linha, column=8).value = 0
 
-# Carregar o arquivo do Excel
-workbook = openpyxl.load_workbook('/home/yudiaramos/Desktop/projetos/trunks-test/inicial.xlsx')  # Substitua pelo caminho real do seu arquivo
+
+workbook = openpyxl.load_workbook('/home/yudiaramos/Desktop/projetos/trunks-test/inicial.xlsx')
 sheet = workbook.active
 
 # Exemplo de chamada da função para calcular a situação do aluno na linha 4
 calcular_situacao_aluno(4)
 
 # Salvar as alterações no arquivo
-workbook.save('/home/yudiaramos/Desktop/projetos/trunks-test/saida-final.xlsx')  # Substitua pelo caminho real do arquivo de saída
+workbook.save('/home/yudiaramos/Desktop/projetos/trunks-test/saida-final.xlsx') 
 
